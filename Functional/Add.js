df40189d9497d1950d9817f4429a7ff6aaf9dd7c
Akash Tambe <akashtambe11@gmail.com>
2020-01-15 17:14:04 +0530

    Modified: Add.js

--- Functional/Add.js
+++ Functional/Add.js
@@ -4,5 +4,6 @@ var util = require('../Utility/FunctionalUtility');
 var num1 = parseInt(read.question("Enter number 1 = \n"));
 var num2 = parseInt(read.question("Enter number 1 = \n"));
 
+// answer is as follows
 var answer = util.add(num1, num2);
 console.log(answer);
